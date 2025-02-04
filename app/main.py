675d77c7d4f3dc9bf0b77acea31e3be2462cdba5
habibulmursaleen@gmail.com <habibulmursaleen@gmail.com>
2025-02-04 14:29:21 +0600

    first commit. created a basic main.py file

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI Chat API"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
